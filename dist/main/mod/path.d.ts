45f62ea3a29acca98649093aac9461d79bb530e9
youliso <youliso@hotmail.com>
2022-04-22 20:38:23 +0800

    feat: del

--- dist/main/mod/path.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function sep(): string;
-export declare function isAbsolute(str: string): boolean;
-export declare function dirname(str: string): string;
-export declare function normalize(str: string): string;
-export declare function basename(str: string): string;
-export declare function pathOn(): void;
--- dist/renderer/mod/path.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function sep(): Promise<string>;
-export declare function isAbsolute(path: string): Promise<string>;
-export declare function dirname(path: string): Promise<string>;
-export declare function normalize(path: string): Promise<string>;
-export declare function basename(path: string): Promise<string>;
