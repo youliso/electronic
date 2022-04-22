ecf826345c679979642f6f006406f3d6ac1f171a
youliso <youliso@hotmail.com>
2022-04-22 21:41:32 +0800

    fix: bug

---
+++ packages/preload/src/index.ts
@@ -0,0 +1,23 @@
+import type { IpcRendererEvent } from 'electron';
+import { contextBridge, ipcRenderer } from 'electron';
+import { EOL } from 'os';
+
+export function preloadInit(isSecondInstanceWin:boolean){
+    contextBridge.exposeInMainWorld('ipc', {
+        send: (channel: string, args?: any) => ipcRenderer.send(channel, args),
+        sendSync: (channel: string, args?: any) => ipcRenderer.sendSync(channel, args),
+        on: (channel: string, listener: (event: IpcRendererEvent, ...args: any[]) => void) =>
+          ipcRenderer.on(channel, listener),
+        once: (channel: string, listener: (event: IpcRendererEvent, ...args: any[]) => void) =>
+          ipcRenderer.once(channel, listener),
+        invoke: (channel: string, args: any) => ipcRenderer.invoke(channel, args),
+        removeAllListeners: (channel: string) => ipcRenderer.removeAllListeners(channel)
+      });
+      
+      contextBridge.exposeInMainWorld('environment', {
+        EOL,
+        systemVersion: process.getSystemVersion(),
+        platform: process.platform,
+        isSecondInstanceWin
+      });
+}
