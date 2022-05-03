515217c4d8c1390ce25a0f924501ab4875672f52
youliso <youliso@hotmail.com>
2022-05-03 23:41:35 +0800

    fix: add dev

--- test/main.js
+++ test/main.js
@@ -6,6 +6,20 @@ appInstance
   .then(() => {
     windowInstance.loadUrl = join(__dirname, "../test/index.html");
 
+    // 调试模式
+    // if (!app.isPackaged) {
+    //   try {
+    //     import('fs').then(({ readFileSync }) => {
+    //       import('path').then(({ join }) => {
+    //         windowInstance.loadUrl = `http://localhost:${readFileSync(join('.port'), 'utf8')}`;
+    //         windowInstance.create(customize, opt);
+    //       });
+    //     });
+    //   } catch (e) {
+    //     throw 'not found .port';
+    //   }
+    // } else windowInstance.create(customize, opt);
+
     windowInstance.create(
       {
         title: "electron-template",
