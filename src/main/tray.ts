3dcd90524e2c863cd5dc2f86481764d38d4f9bff
youliso <youliso@hotmail.com>
2023-01-12 13:55:52 +0800

    update

--- src/main/tray.ts
+++ src/main/tray.ts
@@ -1,33 +1,29 @@
-import { app, Menu, Tray as electronTray, nativeImage } from 'electron';
+import type { MenuItemConstructorOptions, MenuItem } from 'electron';
+import { app, Menu, Tray, nativeImage } from 'electron';
 import { windowInstance } from './window';
 
-export class Tray {
-  public main: electronTray | undefined; //托盘
+export interface TrayOptions {
+  name: string;
+  iconPath: string,
+  menuTemplate: Array<(MenuItemConstructorOptions) | (MenuItem)>
+}
 
-  constructor() {}
+export function getMenuTemplate(items: Array<(MenuItemConstructorOptions) | (MenuItem)>) {
+  return Menu.buildFromTemplate(items || [
+    {
+      label: '显示',
+      click: () => windowInstance.func('show')
+    },
+    {
+      label: '退出',
+      click: () => app.quit()
+    }
+  ]);
+}
 
-  /**
-   * 创建托盘
-   * */
-  create(trayImgPath: string) {
-    const contextMenu = Menu.buildFromTemplate([
-      {
-        label: '显示',
-        click: () => windowInstance.func('show')
-      },
-      {
-        label: '退出',
-        click: () => app.quit()
-      }
-    ]);
-    this.main = new electronTray(nativeImage.createFromDataURL(trayImgPath));
-    this.main.setContextMenu(contextMenu);
-    this.main.setToolTip(app.name);
-    this.main.on('click', () => windowInstance.func('show'));
-  }
-
-  /**
-   * 监听
-   */
-  on() {}
+export function createTray(opt: TrayOptions) {
+  const tray = new Tray(nativeImage.createFromDataURL(opt.iconPath));
+  tray.setContextMenu(getMenuTemplate(opt.menuTemplate));
+  tray.setToolTip(opt.name);
+  return tray;
 }
