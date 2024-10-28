0d8e6ce687062d0288995280dcfa11f31007aade
youliso <youliso@hotmail.com>
2024-10-28 18:09:58 +0800

    fix

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,5 @@
 import { readdirSync, statSync } from 'fs';
-import { resolve, extname } from 'path';
+import { resolve, extname, sep } from 'path';
 import { builtinModules } from 'module';
 import { minify } from 'rollup-plugin-esbuild-minify';
 import nodeResolve from '@rollup/plugin-node-resolve';
@@ -62,10 +62,8 @@ function file(path) {
 
 const flies = file(srcPath).map((e) => e.substring(dPathLength + 4, e.length - 3));
 let config = [];
-let tss = '';
-flies.forEach((path, index) => {
-  if (path.startsWith('types')) return;
-  tss += `src/${path}.ts `;
+flies.forEach((path) => {
+  if (path.startsWith(`types${sep}index`)) return;
   let cfg;
   if (path.startsWith('render')) {
     cfg = {
