4419170762b0a7d3574cc524be3634656b5ca740
youli <youliso@hotmail.com>
2025-03-17 14:05:25 +0800

    build(electronic): 更新 Electron 版本并调整项目结构
    - 将 Electron 版本从 34.3.3 升级到 35.0.2
    - 更新项目结构，移除中间的 index 文件
    - 调整 tsup 配置，同步修改输出路径
    - 版本号从 6.0.0 修改为 6.0.1

--- tsup.config.ts
+++ tsup.config.ts
@@ -10,8 +10,8 @@ export default defineConfig(() => ({
   format: ['esm', 'cjs'],
   entry: {
     index: 'src/index.ts',
-    'main/index': 'src/main/index.ts',
-    'render/index': 'src/render/index.ts',
+    main: 'src/main/index.ts',
+    render: 'src/render/index.ts',
     preload: 'src/preload/index.ts'
   },
   external: [...builtinModules, '../types', 'electron', 'electron-updater', 'builder-util-runtime']
